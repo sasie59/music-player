9ff1ff1114507f3bd88e564e0c867f122885f37f
sasie59 <sasie59@gmail.com>
2020-11-07 16:55:25 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import reducers from "musicPlayer_1/reducers";
+import SongList from 'musicPlayer_1/components/SongList';
+import StatusBlock from 'musicPlayer_1/components/StatusBlock';
+import './style.scss';
+import {
+  // setLoopMode,
+  // pause,
+  // play,
+  // resume,
+  // stop,
+  // isLike,
+  // appendSecond,
+  // setCurrentPlaying,
+  // NONE,
+  // RANDOM,
+  // ALL_PLAY,
+} from "musicPlayer_1/actions";
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+store.subscribe(() => console.warn(store.getState()));
+// store.dispatch(play(5));
+// store.dispatch(appendSecond());
+// store.dispatch(setCurrentPlaying(0));
+// store.dispatch(appendSecond());
+// store.dispatch(isLike(0))
+// store.dispatch(isLike(8))
+// store.dispatch(pause());
+// store.dispatch(resume())
+// store.dispatch(stop())
+// store.dispatch(setLoopMode(ALL_PLAY));
+// store.dispatch(setLoopMode(NONE));
+// store.dispatch(setLoopMode(RANDOM));
+
+export default class index extends Component {
+  
+  startInterval = (interval) => {
+    this.interval = interval;
+  }
+
+  stopInterval = () => {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className='musicPlayer_1'>
+        <Provider store={store}>
+          <SongList
+            startInterval={this.startInterval}
+            stopInterval={this.stopInterval}
+          />
+          <StatusBlock
+            startInterval={this.startInterval}
+            stopInterval={this.stopInterval}
+          />
+        </Provider>
+      </div>
+    )
+  }
 }
-
-export default App;
